3964a9c795e1b905d91bdff2b9246fc3f5079bd6
Patrick Weiss <p4trick.weiss@gmail.com>
2023-05-12 17:09:26 +0200

    tourlog crud works

--- src/main/java/at/technikumwien/tourplanner_backend/service/tourlogservice/TourLogController.java
+++ src/main/java/at/technikumwien/tourplanner_backend/service/tourlogservice/TourLogController.java
@@ -1,12 +1,29 @@
 package at.technikumwien.tourplanner_backend.service.tourlogservice;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import at.technikumwien.tourplanner_backend.model.dto.NewTourLog;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/api/tourlog")
+@RestController
+@RequestMapping("/api/tour-log")
 public class TourLogController {
     private final TourLogService tourLogService;
 
     public TourLogController(TourLogService tourLogService) {
         this.tourLogService = tourLogService;
+    }
+
+    @PostMapping()
+    public void addLog(@RequestBody NewTourLog newTourLog) {
+        this.tourLogService.addTourLog(newTourLog);
+    }
+
+    @PutMapping("/{logId}")
+    public void updateLog(@PathVariable("logId") Long logId, @RequestBody NewTourLog newTourLog) {
+        this.tourLogService.updateTourLog(logId, newTourLog);
+    }
+
+    @DeleteMapping("/{logId}")
+    public void deleteLog(@PathVariable("logId") Long logId) {
+        this.tourLogService.deleteLog(logId);
     }
 }
